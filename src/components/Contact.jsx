399bde0fe3cb20a5cb3ff9d58a195386936e43a3
Nethmi Marasinghe <115603849+NemiYunn@users.noreply.github.com>
2024-02-01 19:27:37 +0530

    Made some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-10'>
         <form action="https://getform.io/f/0685f4cb-c6a5-47e2-9ccc-6154619d82dd" method='POST' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600 '>Contact</p>
